c72d479bfd703397ed617c6243a31e6c1d283a65
qfhuang <1531322066@qq.com>
2014-08-07 22:45:39 +0800

    lisp

--- source/lisp/test.lisp
+++ source/lisp/test.lisp
@@ -19,4 +19,37 @@
   (dolist (cd *db*)
     (format t "~{~a: ~10~a~%~}~%" cd)))
 
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Aritist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p (prompt-read "Ripped [y/n]:"))))
+
+(defun add-cds ()
+  (loop  (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+		       :direction :output
+		       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+
+
+
+
+
 
